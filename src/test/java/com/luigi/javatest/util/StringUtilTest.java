b59a19cc705ec4f2fc65d44622f1f861f5457520
Luis Cacuango <manuel.cacuango@est.umet.edu.ec>
2025-12-18 10:05:53 -0500

    Creación de Tests Unitarios y Excepciones en Java

--- src/test/java/com/luigi/javatest/util/StringUtilTest.java
+++ src/test/java/com/luigi/javatest/util/StringUtilTest.java
@@ -4,15 +4,16 @@ public class StringUtilTest {
 
     public static void main(String[] args) {
 
-        String result = StringUtil.repeat("hola", 3);
-        if (!result.equals("holaholahola")) {
-            System.out.println("ERROR");
-        }
+        assertEquals(StringUtil.repeat("hola", 3), "holaholahola");
 
-        String result2 = StringUtil.repeat("hola", 1);
-        if (!result2.equals("hola")) {
-            System.out.println("ERROR");
-        }
+        assertEquals(StringUtil.repeat("hola", 1), "hola");
 
+    }
+
+    private static void assertEquals(String actual, String expected) {
+
+        if (!actual.equals(expected)) {
+            throw new RuntimeException(actual + " no es igual que " + expected);
+        }
     }
 }
