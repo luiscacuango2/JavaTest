cfe25605c64f61fcbb81c5e22e87d1bff743e5b6
Luis Cacuango <manuel.cacuango@est.umet.edu.ec>
2025-12-18 10:51:22 -0500

    Testing en Java con JUnit para Verificar Contraseñas

---
+++ src/test/java/com/luigi/javatest/util/PasswordUtilTest.java
@@ -0,0 +1,30 @@
+package com.luigi.javatest.util;
+
+import org.junit.Test;
+
+import static com.luigi.javatest.util.PasswordUtil.SecurityLevel.*;
+import static org.junit.Assert.*;
+
+public class PasswordUtilTest {
+
+    @Test
+    public void weak_when_has_less_than_8_letters() {
+        assertEquals(WEAK, PasswordUtil.assessPassword("123aa!"));
+    }
+
+    @Test
+    public void weak_when_has_only_letters() {
+        assertEquals(WEAK, PasswordUtil.assessPassword("abcdefghijk"));
+    }
+
+    @Test
+    public void medium_when_has_only_letters_numbers() {
+        assertEquals(MEDIUM, PasswordUtil.assessPassword("abcde12345"));
+    }
+
+    @Test
+    public void strong_when_has_only_letters_numbers_and_symbols() {
+        assertEquals(STRONG, PasswordUtil.assessPassword("abcd123!"));
+    }
+
+}
