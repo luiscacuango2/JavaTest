b7584befe20576dde9b49717b421cac7facd13f6
Luis Cacuango <manuel.cacuango@est.umet.edu.ec>
2025-12-21 06:27:13 -0500

    Organización y Mejora de Tests en jUnit. True o False, poniendo variables globales

--- src/test/java/com/luigi/javatest/payments/PaymentProcessorTest.java
+++ src/test/java/com/luigi/javatest/payments/PaymentProcessorTest.java
@@ -1,5 +1,6 @@
 package com.luigi.javatest.payments;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -7,29 +8,32 @@ import static org.junit.Assert.*;
 
 public class PaymentProcessorTest {
 
+    private PaymentGateway paymentGateway;
+    private PaymentProcessor paymentProcessor;
+
+    @Before
+    public void setup() {
+        paymentGateway = Mockito.mock(PaymentGateway.class);
+        paymentProcessor = new PaymentProcessor(paymentGateway);
+    }
+
     @Test
     public void payment_is_correct() {
 
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.requestPayment(Mockito.any()))
-                .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
+                .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
 
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
-
-        assertFalse(paymentProcessor.makePayment(1000));
+        assertTrue(paymentProcessor.makePayment(1000));
 
     }
 
     @Test
     public void payment_is_wrong() {
 
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.requestPayment(Mockito.any()))
-                .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
+                .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
 
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
-
-        assertTrue(paymentProcessor.makePayment(1000));
+        assertFalse(paymentProcessor.makePayment(1000));
 
     }
 }
